26d20ee03aeac16d8bf2f96ba57de33093cca69b
JamesTovarR04 <jamestovar09@hotmail.com>
2026-08-16 01:12:06 -0500

    actualizar contenido y enlaces en la invitación digital, ajustar estilos y agregar metadatos para SEO

--- src/app/components/confirm/confirm.ts
+++ src/app/components/confirm/confirm.ts
@@ -8,4 +8,9 @@ import { ScrollAnimate } from '../../shared/scroll-animate';
   styleUrl: './confirm.scss',
   imports: [Whatsapp, ScrollAnimate],
 })
-export class Confirm {}
+export class Confirm {
+  mensaje =
+    'Hola, confirmo mi asistencia a la boda. ¡Gracias por la invitación!';
+  urlCarlos = `https://wa.me/573223400203?text=${encodeURIComponent(this.mensaje)}`;
+  urlValentina = `https://wa.me/573224015907?text=${encodeURIComponent(this.mensaje)}`;
+}
