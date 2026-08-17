6540f8f604adf66c1846ad345e4372241e29eed4
JamesTovarR04 <jamestovar09@hotmail.com>
2026-08-17 01:21:05 -0500

    agregar funcionalidad de generación de código QR en la página de recuerdos; actualizar dependencias y modificar la plantilla

--- src/app/components/memories/memories.ts
+++ src/app/components/memories/memories.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Card } from '../../shared/card/card';
 import { Camara } from '../animaciones/camara';
 import { ActivatedRoute } from '@angular/router';
+import QRCode from 'qrcode';
 
 @Component({
   selector: 'app-memories',
@@ -9,13 +10,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './memories.scss',
   imports: [Card, Camara],
 })
-export class Memories {
+export class Memories implements OnInit {
+  @ViewChild('qrCanvas', { static: true }) qrCanvas!: ElementRef<HTMLCanvasElement>;
+
   urlAlbum = '';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const guid = this.route.snapshot.paramMap.get('guid');
-    this.urlAlbum = `../album/${guid}`;
+    this.urlAlbum = `${window.location.origin}/album/${guid}`;
+
+    QRCode.toCanvas(this.qrCanvas.nativeElement, this.urlAlbum, {
+      width: 135,
+      margin: 1,
+      color: { dark: '#1a243f', light: '#ffffff' },
+    });
   }
 }
